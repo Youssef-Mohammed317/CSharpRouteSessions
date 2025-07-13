2c941bad93ad412eaecf40d36bdaff3775568a09
Youssef-Mohammed317 <youssefmohammad040317@gmail.com>
2025-07-13 21:09:03 +0300

    ready for assignment

--- Program.cs
+++ Program.cs
@@ -1,10 +1,97 @@
-﻿namespace CSharpRouteSessions
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CSharpRouteSessions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 
+            //1 - Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            //Example
+            //Input: 5
+            //Output: 1, 2, 3, 4, 5 
+
+            #endregion
+
+            #region Q2
+            //2 - Write a program that allows the user to insert an integer then
+            //print a multiplication table up to 12.
+            //Example
+            //Input: 5
+            //Outputs: 5 10 15 20 25 30 35 40 45 50 55 60
+            #endregion
+
+            #region Q3
+            //3 - Write a program that allows to user to insert number then print all even numbers between 1 to this number
+            //Example:
+            //Input: 15
+            // Output: 2 4 6 8 10 12 14
+            #endregion
+
+            #region Q4
+            //4 - Write a program that takes two integers then prints the power.
+            //Example:
+            //Input: 4 3
+            //Output: 64
+            //Hint: how to calculate 4 ^ 3 = 4 * 4 * 4 = 64
+            #endregion
+
+            #region Q5
+            //5 - Write a program to enter marks of five subjects and calculate total, average and percentage.
+            //Example
+            //Input: -Enter Marks of five subjects: 95 76 58 90 89
+            //Output: Total marks = 408
+            //Average Marks = 81
+            //Percentage = 81
+
+            #endregion
+
+            #region Q6
+            //6 - Write a program to allow the user to enter a string and print the REVERSE of it.
+            #endregion
+
+            #region Q7
+            //7 - Write a program to allow the user to enter int and print the REVERSED of it.
+            #endregion
+
+            #region Q8
+            //8 - Write a program in C# Sharp to find prime numbers within a range of numbers.
+            //Test Data:
+            //            Input starting number of ranges: 1
+            //Input ending number of range: 50
+
+            //Expected Output :
+            //The prime number between 1 and 50 are:
+            //            2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+
+            #endregion
+
+            #region Q9
+            //9 - .Write a program in C# Sharp to convert a decimal number into binary without using an array.
+            //Test Data:
+            //            Enter a number to convert: 25
+            //Expected Output :
+            //The Binary of 25 is 11001.
+
+
+            #endregion
+
+            #region Q10
+            //10 - Create a program that asks the user to input three points(x1, y1), (x2, y2), and(x3, y3),
+            //and determines whether these points lie on a single straight line.
+            #endregion
+
+            #region Q11
+            //11 - .Write a program that prints an identity matrix using for loop,
+            //in other words takes a value n from the user and shows the identity table of size n * n.
+            #endregion
+
         }
     }
 }
